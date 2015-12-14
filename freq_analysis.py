09aee459276a71c45e47beea292e1f3028ce4c3a
ychen810811 <chen.elliott@gmail.com>
2015-12-14 18:24:41 +0800

    In function letter_sorted_per_freq(), sorted letters with same freq in reversed ETAOIN order

--- freq_analysis.py
+++ freq_analysis.py
@@ -4,7 +4,12 @@ import pprint
 
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+FREQ_ENGLISH = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I':
+6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C':
+2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P':
+1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z':
+0.07}
 
 def letter_count(string):
     letter_occurrences = {}
@@ -28,15 +33,23 @@ def letter_sorted_per_freq(string):
             flag_insert = True
             i = 0
             while flag_insert:
-                if freq_letters[c] >= freq_letters[letter_sorted[i]]:
+                if freq_letters[c] > freq_letters[letter_sorted[i]]:
                     letter_sorted.insert(i, c)
                     flag_insert = False
+                elif freq_letters[c] == freq_letters[letter_sorted[i]]:
+                    if ETAOIN.find(c) > ETAOIN.find(letter_sorted[i]):
+                        letter_sorted.insert(i, c)
+                        flag_insert = False
+                    else:
+                        i += 1
                 elif i == len(letter_sorted) - 1:
                     letter_sorted.append(c)
                     flag_insert = False
                 else:
                     i += 1
     return ''.join(letter_sorted)
+
+    # Yet there is another awkard implementation making use several compound objects
 
 
 def freq_match_score(string):
