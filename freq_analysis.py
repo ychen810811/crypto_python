664f88b7e5550b66bf01f5e7129ac138a7fc4944
ychen810811 <chen.elliott@gmail.com>
2015-12-16 12:56:20 +0800

    Implemented freq_match_score() to return frequency matching score of a string

--- freq_analysis.py
+++ freq_analysis.py
@@ -4,11 +4,12 @@ import pprint
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
-FREQ_ENGLISH = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I':
-6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C':
-2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P':
-1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z':
-0.07}
+FREQ_ENGLISH = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97,
+                'N': 6.75,  'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25,
+                'L': 4.03,  'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36,
+                'F': 2.23,  'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29,
+                'V': 0.98,  'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10,
+                'Z': 0.07}
 
 
 def letter_count(string):
@@ -79,7 +80,20 @@ def _return_key_value(x):
 
 
 def freq_match_score(string):
-    pass
+    match_score = 0
+    freq_string = letter_sorted_per_freq(string)
+    for c in freq_string[:6]:
+        print(c)
+        if c in ETAOIN[:6]:
+            match_score += 1
+            print('match_score = %s' % (match_score))
+    for c in freq_string[-6:]:
+        print(c)
+        if c in ETAOIN[-6:]:
+            match_score += 1
+            print('match_score = %s' % (match_score))
+    return match_score
+
 
 
 # below is testing code for in-place test functions/methods
@@ -141,5 +155,6 @@ grant Turing a statutory pardon if enacted."""
 pprint.pprint(letter_count(message))
 pprint.pprint(letter_sorted_per_freq(message))
 pprint.pprint(letter_sorted_per_freq_2(message))
+pprint.pprint(freq_match_score(message))
 # pprint.pprint(letter_count(LETTERS))
 # pprint.pprint(letter_sorted_per_freq(LETTERS))
