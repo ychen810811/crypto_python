bdac3fdeec010fd0b67727ff42ac2d5de1ab9be6
ychen810811 <chen.elliott@gmail.com>
2015-12-24 13:57:38 +0800

    Expanded cryptomath module with new functions: isprime() and sieveprime()

--- cryptomath.py
+++ cryptomath.py
@@ -2,6 +2,12 @@
 # Description: This module contains some useful functions in crypto maths
 # gcd(a, b)             - Greatest Common Devisor of a and b
 # findModInverse(a, m)  - Calculate the a's reverse mod m
+# isprime(a)            - Test a integer prime or not
+# sieveprime(size)      - Generate a list of primes with Eratosthenes sieve of
+#                         size
+
+import math
+import pprint
 
 
 def gcd(a, b):
@@ -21,5 +27,35 @@ def findModInverse(a, m):
     return u1 % m
 
 
+# Return True is a is prime, False otherwise
+def isprime(a):
+    if a < 2:
+        return False
+    for i in range(2, int(math.sqrt(a)) + 1):
+        if a % i == 0:
+            return False
+    return True
+
+
+# Return a list of prime numbers with the Eratosthenes sieve of the size
+def sieveprime(size):
+    sieve = [True] * size
+    sieve[0], sieve[1] = False, False
+    for i in range(2, int(math.sqrt(size)) + 1):
+        ptr = i * 2
+        while ptr < size:
+            sieve[ptr] = False
+            ptr += i
+
+    p = []
+    for i in range(size):
+        if sieve[i]:
+            p.append(i)
+    return p
+
+
 if __name__ == '__main__':
     print('This is a cryptographic math library')
+
+    # Testing code
+    pprint.pprint(sieveprime(10000))
